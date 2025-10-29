20a8a85d5413b50ea6420fd3f45d7f9896276b9a
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-10-29 12:55:23 +0500

    Task0

--- Tyuiu.ArkhipovaMD.Sprint5.Task0.V26.Lib/DataService.cs
+++ Tyuiu.ArkhipovaMD.Sprint5.Task0.V26.Lib/DataService.cs
@@ -1,12 +1,15 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
-using System.IO;
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace Tyuiu.ArkhipovaMD.Sprint5.Task0.V26.Lib
 {
     public class DataService : ISprint5Task0V26
     {
         public string SaveToFileTextData(int x)
         {
-            string path = $@"{Directory.GetCurrentDirectory()}\OutputFileTask0.txt";
+            //string dir = Path.Combine(Environment.CurrentDirectory, "AppData");
+            //Directory.CreateDirectory(dir);
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "OutputFileTask0.txt");
             double fx = Math.Round(0.7*Math.Pow(x, 3)+1.52*Math.Pow(x,2),3);
             File.WriteAllText(path,Convert.ToString(fx));
             return path;
