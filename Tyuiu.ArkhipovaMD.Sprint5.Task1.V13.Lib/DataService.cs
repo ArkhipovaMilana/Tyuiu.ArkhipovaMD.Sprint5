6848e7741e1dca56bedcbd9342ccb441f48e98d1
Milana Arkhipova <Tyuiu.ArkhipovaMD@outlook.com>
2025-11-01 00:07:14 +0500

    Task1

--- Tyuiu.ArkhipovaMD.Sprint5.Task1.V13.Lib/DataService.cs
+++ Tyuiu.ArkhipovaMD.Sprint5.Task1.V13.Lib/DataService.cs
@@ -6,22 +6,27 @@ namespace Tyuiu.ArkhipovaMD.Sprint5.Task1.V13.Lib
         public string SaveToFileTextData(int startValue, int stopValue)
         {
             string path = Path.Combine(Path.GetTempPath(), "OutputFileTask1.txt");
-            int len = stopValue - startValue + 1;
+            FileInfo fileInfo = new FileInfo(path);
+            if (fileInfo.Exists)
+            {
+                fileInfo.Delete();
+            }
             double y;
-            string stry="i";
+            string stry="";
             for (int x = startValue; x <= stopValue; x++)
             {
                 if (Math.Cos(x) == -x)
                 {
                     y = 0;
-                    Convert.ToString(y);
+                    stry += Convert.ToString(y)+"\n";
                 }
                 else
                 {
                     y = Math.Round(2*x-3/ (Math.Cos(x) + x), 2);
-                    stry = Convert.ToString(y);
+                    stry += Convert.ToString(y) + "\n";
                 }
-                File.WriteAllText(path,stry+"\n");
+                Console.WriteLine(stry);
+                File.WriteAllText(path, stry);
             }
             return path;
         }
--- Tyuiu.ArkhipovaMD.Sprint5.Task1.V13/Program.cs
+++ Tyuiu.ArkhipovaMD.Sprint5.Task1.V13/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Tyuiu.ArkhipovaMD.Sprint5.Task1.V13.Lib;
 
 DataService ds  = new DataService();
-Console.WriteLine(ds.SaveToFileTextData);
+Console.WriteLine(ds.SaveToFileTextData(-5,5));
